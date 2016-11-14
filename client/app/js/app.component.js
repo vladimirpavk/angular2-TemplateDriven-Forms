a708fc8e14f8323c1442fb80257d3ce7c02aefe8
Vladimir Pavković <vladimirpavk@gmail.com>
2016-11-14 13:34:44 +0100

    example from angular.io/forms

--- client/app/js/app.component.js
+++ client/app/js/app.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(['@angular/core', './hero'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,17 +10,30 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, hero_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (hero_1_1) {
+                hero_1 = hero_1_1;
             }],
         execute: function() {
             let AppComponent = class AppComponent {
-                submit(form_value) {
-                    console.log(form_value);
+                onSubmit() {
+                    this.submitted = true;
+                }
+                // TODO: Remove this when we're done
+                get diagnostic() { return JSON.stringify(this.model); }
+                clickSubmit(event) {
+                    console.log(event);
+                }
+                ngOnInit() {
+                    this.powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+                    this.model = new hero_1.Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+                    this.submitted = false;
                 }
             };
             AppComponent = __decorate([
--- client/app/ts/app.component.ts
+++ client/app/ts/app.component.ts
@@ -1,14 +1,34 @@
 /// <reference path="../../../node_modules/@angular/common/index.d.ts" />
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Hero } from './hero';
+
 @Component({
   selector: 'my-app',
   templateUrl: './client/app/ts/app.component.html'
 })
-export class AppComponent { 
+export class AppComponent implements OnInit{ 
 
-  public username:string;
+  public powers: Array<string>;
+  public model: Hero;
+  public submitted: boolean;
 
-  public submit(form_value:any):void{
-    console.log(form_value);
+  public onSubmit(): void
+  { 
+     this.submitted = true; 
   }
+  
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  public clickSubmit(event):void{
+    console.log(event);
+  }
+
+  ngOnInit(){
+    this.powers= ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+    this.model= new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+    this.submitted=false;
+  }
+
 }
